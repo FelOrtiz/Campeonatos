60a6e192535b07f41a173d04f9fcdd1b3a56ce09
FelOrtiz <holex_15@hotmail.com>
2015-09-23 15:00:20 -0300

    primer commit

---
+++ Contru.campaonatos/Controllers/JugadorController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Contru.campaonatos.Controllers
+{
+    public class JugadorController : Controller
+    {
+        //
+        // GET: /Jugador/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public ActionResult Teams()
+        {
+            return View();
+        }
+        public ActionResult Tournaments()
+        {
+            return View();
+        }
+    }
+}
---
+++ Contru.campaonatos/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Contru.campaonatos.MvcApplication" Language="C#" %>
